eb97790f2c4cdec1d7edf9151759cd1000b53dc8
Guilherme <borutoplays038@gmail.com>
2026-09-21 08:33:48 -0300

    feat: Atualizando o projeto da aula passada

--- backend/dicas.md
+++ backend/dicas.md
@@ -1,13 +1,14 @@
-# 1 - CRIAR A PASTA BACKEND
-# 2 - DENTRO DA PASTA, CRIE O ARQUIVO
+# 1- criar a pasta backend
+mkdir backend
+# 2- dentro da pasta criar o arquivo
 server.js
-# 3 - CRIAR O ARQUIVO PACKAGE.JSON
+# 3- criar o arquivo package.json
 npm init -y
-# 4 - INSTALAR A DEPENDÊNCIA DO EXPRESS
-npm install expres
-# 5 - INSTALAR A DEPENDÊNCIA DE REQUISIÇÃO DE SERVIDOR (DOMÍNIOS)
+# 4- instalar a dependencia do express
+npm install express
+# 5- instalar a dependencia de requisição de servidor (dominios)
 npm install cors
-# 6 INSTALA A DEPENDÊNCIA PARA DEIXAR O SERVIDOR RODANDO SEMPRE
+# 6- instala dependencia para deixar servidor rodando sempre
 npm install nodemon
-acesse o arquivo package.json e mude o "start": de "node server.js" para "nodemon server.js"
-e no terminal digite: npm run start
+muda o "start": "nodemon server.js
+e no terminal digita: npm run start
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,65 @@
-// IMPORTAR O MODULO EXPRESS - FRAMEWORK DE APLICACAO WEB PARA NODE.JS
-const express = require('express');
-// MÓDULO QUE PERMITE QUE O SERVIDOR ACEITE REQUISIÇÕES DIFERENTES (DOMÍNIO)
-const cors = require('cors');
-// INSTANCIANDO EXPRESS PARA APP (Atribuindo o nome app ao express)
-const app = express();
-// DEFININDO A PORTA QUE O SERVIDOR SERÁ EXECUTADO
-const port = 3001;
+// importar o modulo express- framework de aplicação web para node.js
+const express = require('express')
 
-//Configura o express para analisar as requisições com o corpo no formato json, isso é necessário,
-//para ler os dados enviados no corpo da requisição POST
+// modulo que permite que o servidor aceite requisições diferentes (dominios)
+const cors = require('cors')
+
+// instanciando express para app
+const app = express();
+
+const port=3001;
+
+
+//configura o express para analisar as requisições com o corpo no formato json
+// corpo no formato json, isso é necessário para ler os dados enviados
+// no corpo da requisição POST
 app.use(express.json());
 
-//HABILITA O CORS PARA TODAS AS ROTAS DA APLICAÇÃO,
-app.use(cors());
+//habilita o cors para todas as rotas da aplicação permitindo o acesso
+app.use(cors())
 
+//objeto (tabela com os preços)
 const precos={
-    bicicleta:0.75,
-    carro: .25,
-    drone: 1.20
+    bicileta:0.75, //preço por km para bicicleta
+    carro:0.25, // preço por km para bicicleta
+    drone:1.20 // preço por km para drone
 }
 
-app.post('/CalcularFrete',(req,res)=>{
-    
-    const{distancia,tipoTransporte}=req.body;
+// definindo uma rota de API tipo POST
 
-    if (distancia === undefined || tipoTransporte === undefined){
-        return res.status();
+// função de callback lida com requisição
+
+app.post('/calcularfrete',(req,res)=>{
+    //destruct para o corpo da requisição e extrair distancia e tipoTransporte
+    const {distancia,tipoTransporte}= req.body;
+
+    // verifica se a distancia ou tipoTransporte não foram fornecidos
+    if(distancia === undefined || tipoTransporte === undefined){
+        return res.status(400).json({error: 'Distancia e tipo de transporte são obrigatorios'})
+
     }
+
+    // Busca o preço por KM no objeto convertendo o tipo de transporte para minusculos
+    const precoPorKm = precos[tipoTransporte.toLowerCase()];
+
+
+    // verifica se o tipoTransporte fornecido existe na tabela de preços
+    if(precoPorKm === undefined){
+        return.res.status(400).json({error: "Tipo de transporte inválido"})
+    }
+
+
+    // calcula o valor total do frete multiplicando a distancia pelo preço por km
+    const valorTotal = distancia * precoPorKm;
+    // Envia a resposta com o objeto JSON
+    // toFixed - formata o valor total para ter exatamente duas casas decimais
+    res.json({valorTotal: valorTotal.toFixed(2)})
+
 })
 
-// INICIA O SERVIDOR PARA QUE ELE COMECE A ESCUTAR AS REQUISIÇÕES 
+//inicia o servidor para que ele comece a escutar as requisições na porta
 app.listen(port,()=>{
-    console.log(`Servidor rodando na porta http://localhost:${port}`);
+    console.log(`Servidor ROdando na porta http://localhost:${port}`);
 })
+
+
